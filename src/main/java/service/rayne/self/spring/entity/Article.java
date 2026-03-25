4b1b7a5d69c53beb32a8ab6bc4a2282cb7a41f23
Rayne <pluvstream@gmail.com>
2026-03-26 00:41:20 +0900

    Complete Chapter 09

--- src/main/java/service/rayne/self/spring/entity/Article.java
+++ src/main/java/service/rayne/self/spring/entity/Article.java
@@ -1,9 +1,6 @@
 package service.rayne.self.spring.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +12,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class Article {
-  @Id @GeneratedValue
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   @Column
   private String title;
